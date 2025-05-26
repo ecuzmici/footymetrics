e7c099edb6809dbcad40eb127e0958a864523c11
Emil <ecuzmici@gmail.com>
2025-05-26 18:25:28 +0200

    adds favicon to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "FootyMetrics",
   description: "Personal project by Emil Cuzmici",
+  icons: {
+    icon: './favicon.svg'
+  }
 };
 
 export default function RootLayout({ children }) {
